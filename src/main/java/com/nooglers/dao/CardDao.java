d3ab01c0e001e421b161e34024a39fbbc518d8ce
Infinity1441 <jumabekova.baxtigul@gmail.com>
2023-02-14 17:00:26 +0500

    cardDao added

---
+++ src/main/java/com/nooglers/dao/CardDao.java
@@ -0,0 +1,34 @@
+package com.nooglers.dao;
+
+import com.nooglers.domains.Card;
+
+import java.util.List;
+
+public class CardDao extends BaseDao<Card,Integer>{
+
+    @Override
+    protected Card save(Card card) {
+
+        return null;
+    }
+
+    @Override
+    protected Card update(Card card) {
+        return null;
+    }
+
+    @Override
+    protected Card delete(Integer integer) {
+        return null;
+    }
+
+    @Override
+    protected List<Card> getAll() {
+        return null;
+    }
+
+    @Override
+    protected Card get(Integer integer) {
+        return null;
+    }
+}
