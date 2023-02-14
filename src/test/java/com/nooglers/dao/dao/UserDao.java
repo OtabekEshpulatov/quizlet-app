b72c89ac4735e4373ac69275be1c57d8209c2190
otabek <otabekeshpulatov123@gmail.com>
2023-02-14 15:43:08 +0500

    adding functionality to UserDao p3

---
+++ src/test/java/com/nooglers/dao/dao/UserDao.java
@@ -0,0 +1,39 @@
+package com.nooglers.dao.dao;
+
+import com.nooglers.domains.User;
+
+public class UserDao {
+    public static void main(String[] args) {
+
+
+//        saveTest();
+//        updateTest();
+
+    }
+
+    private static void updateTest() {
+        com.nooglers.dao.UserDao dao = new com.nooglers.dao.UserDao();
+
+        User polarBear = dao.update(User.builder()
+                .username("polarBear")
+                .id(2)
+                .build());
+
+        System.out.println(polarBear);
+    }
+
+    private static void saveTest() {
+        com.nooglers.dao.UserDao dao = new com.nooglers.dao.UserDao();
+
+
+        User user = dao.save(User.builder()
+                .username("polar")
+                .email("polar@gmail.com")
+                .password("123")
+                .build());
+
+        dao.save(user);
+    }
+
+
+}
