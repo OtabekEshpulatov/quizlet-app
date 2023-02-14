81ee5cc8da408d349b7ac505b7058954238a24c7
otabek <otabekeshpulatov123@gmail.com>
2023-02-14 17:51:33 +0500

    adding view

--- src/main/java/com/nooglers/servlets/HomeServlet.java
+++ src/main/java/com/nooglers/servlets/HomeServlet.java
@@ -1,6 +1,9 @@
 package com.nooglers.servlets;
 
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 
+@WebServlet( name = "HomeServlet", urlPatterns = "/home" )
 public class HomeServlet extends HttpServlet {
+
 }
