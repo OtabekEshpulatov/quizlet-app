e2d3a4007f4a4902a616b49a24d2665d9af8598e
otabek <otabekeshpulatov123@gmail.com>
2023-02-17 09:37:59 +0500

    completion of tests p1

--- src/main/java/com/nooglers/dto/SolveQuestionDto.java
+++ src/main/java/com/nooglers/dto/SolveQuestionDto.java
@@ -4,5 +4,5 @@ import com.nooglers.domains.test.Variant;
 
 import java.util.List;
 
-public record SolveQuestionDto( String quizType , String definition , List<Variant> variants , int id ) {
+public record SolveQuestionDto( String quizType , String definition , List<Variant> variants , int id,int totalQuestionCount,int currentQuestionCount ) {
 }
